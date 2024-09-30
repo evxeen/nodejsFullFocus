21a859542639f38a061988033236f40c70f0d732
evxeen <evxeen@gmail.com>
2024-09-30 10:10:30 +0300

    less 5

--- app_01.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-    console.log('Server started!');
-    res.end('1');
-}).listen(3500);
--- app_05/app.js
+++ app_05/app.js
@@ -12,16 +12,30 @@ http.createServer((req, res) => {
         case '/':
             console.log('mane page is running!');
             res.write('<h1>Mane page</h1>');
+            res.end();
             break;
         case '/contact':
             console.log('contacts');
             let data = fs.readFileSync('./public/contact.html', {encoding: 'utf-8', flag: 'r'});
             res.write(data);
+            res.end();
             break;
         default:
-            console.log('404 page');
-            res.write('<h1>404 page</h1>');
-            break;
+            if(url.includes('/images')) {
+                console.log('images =>>>>>>>>>')
+                fs.readFile('./public/' + url, {}, (err, data) => {
+                    if (err) {
+
+                    }
+                    console.log('=========get image')
+                    res.setHeader("Content-Type", "image/png");
+                    res.write(data);
+                    res.end();
+                });
+            } else {
+                console.log('404 page');
+                res.write('<h1>404 page</h1>');
+                res.end();
+            }
     }
-    res.end();
 }).listen(PORT);
